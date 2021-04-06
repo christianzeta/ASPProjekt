b6fe448b76b3a7e93c07936337b2c888e6c2218c
David Rask <david.rask@hotmail.com>
2021-04-06 14:40:17 +0200

    inverseProperty

--- ASPProjekt/Data/ApplicationDbContext.cs
+++ ASPProjekt/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using ASPProjekt.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ASPProjekt.Data
 {
--- ASPProjekt/Models/User.cs
+++ ASPProjekt/Models/User.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASPProjekt.Models
 {
@@ -10,7 +11,10 @@ namespace ASPProjekt.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [InverseProperty("Organizer")]
         public List<Event> HostedEvents { get; set; }
+        [InverseProperty("Attendees")]
         public List<Event> JoinedEvents { get; set; }
     }
 }
