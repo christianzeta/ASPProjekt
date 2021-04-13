2e5da7c94849f560c3d215bc99e34a0ce0d55ddc
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-04-13 12:40:51 +0200

    Working Organizer Functionality

---
+++ ASPProjekt/Pages/CreateEvent.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ASPProjekt.Data;
+using ASPProjekt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPProjekt.Pages
+{
+    [Authorize(Roles="organizer")]
+    public class CreateEventModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+
+
+        public CreateEventModel(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public User user { get; set; }
+
+        [BindProperty]
+        public Event Event { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //var userId = _userManager.GetUserId(User);
+            var username = User.Identity.Name;
+            user = await _userManager.FindByNameAsync(username);
+            Event.Organizer = user;
+            _context.Events.Add(Event);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ASPProjekt/Pages/OrganizerEvents.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASPProjekt.Data;
+using ASPProjekt.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ASPProjekt.Pages
+{
+    [Authorize(Roles="organizer")]
+    public class OrganizerEventsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+
+        public OrganizerEventsModel(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+
+        }
+
+        public IList<Event> Event { get;set; }
+        public User user { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var username = User.Identity.Name;
+            user = await _userManager.FindByNameAsync(username);
+            Event = await _context.Events.Where(o => o.Organizer == user).ToListAsync();
+        }
+    }
+}
